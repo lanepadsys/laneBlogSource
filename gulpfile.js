e3a7f5f7f5593f3985fcb8779d5be0c0fb774a6e
lanepadsys <lanepad@163.com>
2020-09-04 09:44:51 +0800

    ss

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,54 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    cssmin = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin');
-//JS压缩
-gulp.task('uglify', function() {
-    return gulp.src('././public/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('././public/js/'));
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+
+// 获取 gulp-imagemin 模块
+var imagemin = require('gulp-imagemin')
+
+// 压缩 public 目录 css
+gulp.task("default", 'minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
 });
-//public-fancybox-js压缩
-gulp.task('fancybox:js', function() {
-    return gulp.src('././public/fancybox/jquery.fancybox.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('././public/fancybox/'));
-});
-//public-fancybox-css压缩
-gulp.task('fancybox:css', function() {
-    return gulp.src('././public/fancybox/jquery.fancybox.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('././public/fancybox/'));
-});
-//CSS压缩
-gulp.task('cssmin', function() {
-    return gulp.src('././public/css/style.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('././public/css/'));
-});
-//图片压缩
-gulp.task('images', function() {
-    gulp.src('././public/img/*.*')
-        .pipe(imagemin({
-            progressive: false
+// 压缩 public 目录 html
+gulp.task("default", 'minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
         }))
-        .pipe(gulp.dest('././public/img/'));
+        .pipe(gulp.dest('./public'))
 });
-gulp.task('build', ['uglify', 'cssmin', 'images', 'fancybox:js', 'fancybox:css']);
+// 压缩 public/js 目录 js
+gulp.task("default", 'minify-js', function() {
+    return gulp.src('./public/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+
+
+// 压缩图片任务
+// 在命令行输入 gulp images 启动此任务
+gulp.task("default", 'images', function () {
+    // 1. 找到图片
+    gulp.src('./photos/*.*')
+    // 2. 压缩图片
+        .pipe(imagemin({
+            progressive: true
+        }))
+        // 3. 另存图片
+        .pipe(gulp.dest('dist/images'))
+});
+
+
+
+// 执行 gulp 命令时执行的任务
+gulp.task('default', [
+    'minify-html','minify-css','minify-js','images'
+]);
